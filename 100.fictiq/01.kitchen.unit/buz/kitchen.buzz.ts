e970e56a22a6e7f8200c469707682fe24d62f8be
Brad Henderson <elliotbradly@gmail.com>
2025-04-01 11:10:13 -0400

    update

--- 100.fictiq/01.kitchen.unit/buz/kitchen.buzz.ts
+++ 100.fictiq/01.kitchen.unit/buz/kitchen.buzz.ts
@@ -1,21 +1,62 @@
-export const initKitchen = (cpy: KitchenModel, bal:KitchenBit, ste: State) => {
- 
+const { spawn, exec } = require('child_process');
+const path = require('path');
+
+export const initKitchen = (cpy: KitchenModel, bal: KitchenBit, ste: State) => {
+
     bal.slv({ intBit: { idx: "init-kitchen" } });
 
- return cpy;
+    return cpy;
 };
 
-export const openKitchen = (cpy: KitchenModel, bal:KitchenBit, ste: State) => {
-    
-    
+export const openKitchen = (cpy: KitchenModel, bal: KitchenBit, ste: State) => {
+
+    process.chdir('../');
+    process.chdir('./kitchen');
+    process.chdir('./diffusion');
+    launchBatchFile('D:/kitchen/diffusion/webui.bat');
+    process.chdir('../');
+    process.chdir('../');
+    process.chdir('../000.fictiq.services');
+
+
     bal.slv({ intBit: { idx: "open-kitchen" } });
 
     return cpy;
-   };
-
-export const updateKitchen = (cpy: KitchenModel, bal:KitchenBit, ste: State) => {
- return cpy;
 };
+
+export const updateKitchen = (cpy: KitchenModel, bal: KitchenBit, ste: State) => {
+    return cpy;
+};
+
+
+function launchBatchFile(userInputPath) {
+    const sanitizedPath = path.normalize(userInputPath); // Sanitize the path
+
+    const batch = spawn('cmd', ['/c', sanitizedPath], {
+        detached: true,
+        stdio: 'ignore',
+        shell: true
+    });
+
+    //batch.stdout.on('data', (data) => {
+    //  console.log(`stdout: ${data}`);
+    //});
+
+    //batch.stderr.on('data', (data) => {
+    //  console.error(`stderr: ${data}`);
+    //});
+
+    //batch.on('close', (code) => {
+    //  console.log(`child process exited with code ${code}`);
+    //});
+
+    //app.on('before-quit', () => {
+    //  console.log('CLOSE OUT: ' + batch);
+    // batch.kill('SIGKILL');
+    //});
+
+    console.log('Batch file launched!');
+}
 
 
 import { KitchenModel } from "../kitchen.model";
